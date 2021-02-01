0af9be25bb28e4690aa4c41e02b5f05e467128e1
Mobilpymes <whuera@gmail.com>
2021-02-01 14:13:33 -0500

    first commit

---
+++ src/main/java/com/app/lambda/demo/LambdaExpressionApplication.java
@@ -0,0 +1,13 @@
+package com.app.lambda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaExpressionApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (LambdaExpressionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/lambda/demo/LambdaExpressionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.lambda.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaExpressionApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
